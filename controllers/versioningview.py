68cc19a6b50c73025cfa2b8339228653497f79c9
Victor Girotto <victor.girotto@outlook.com>
2018-03-07 20:23:59 -0700

    Refactoring versioning view

--- controllers/versioningview.py
+++ controllers/versioningview.py
@@ -6,60 +6,48 @@ Controller for handling the back end of the versioning view
 
 def get_versioning_structure():
     ''' 
-    Return the structure for the versioning panel 
+    Return the tree structure for the versioning panel 
     Structure:
     [
-        [{ <-- Level 0 start
-            id:31, connections: [{ids:[43,57], type:'merge'}, ...]
-         }, 
-         {
-            id:..., connections: [...]
-         }],     
-        [ <-- Level 1 start
-            ...
-        ],     
-        ...
+        {
+            id:31,
+            type: 'combination',
+            tags: ['tag1', 'tag2'], 
+            children: [
+                {
+                    id: 01,
+                    ...,
+                    children: ...
+                },
+                {
+                    id: 91,
+                    ...,
+                    children: ...
+                }
+            ]
+        }
     ]
-
     '''
     problem_id = session.problem_id
     # get all ideas
-    results = db((db.idea.id == db.tag_idea.idea) & 
-               (db.tag.id == db.tag_idea.tag) & 
-               (db.idea.problem == problem_id)
-    ).select(orderby=db.idea.id, groupby=db.idea.id)
-    
-    # start aux variables
-    levels_map = dict() # key: idea_id, value: level
-    levels = []
-    # Iterate over each result and build the levels array
+    query = (db.idea.id == db.tag_idea.idea) & (db.tag.id == db.tag_idea.tag) & (db.idea.problem == problem_id)
+    results = db(query).select(orderby=db.idea.id, groupby=db.idea.id)
+    # build idea map
+    complete_idea_map = dict()
+    filtered_idea_list = []
     for r in results:
-        sources = r.idea.sources
-        
-        level = __get_level(r.idea.sources, levels_map)
-        # add level to dictionary
-        levels_map[r.idea.id] = level
-        if level > len(levels)-1: # this is the first idea in a new level
-            levels.append([])
-        # add idea to level list
-        levels[level].append(dict(
-            id = r.idea.id,
-            connection = dict(
-                type= r.idea.origin,
-                ids = r.idea.sources
-            )
-        ))
-        # clean_ideas = [dict(id=i.idea.id, idea=i.idea.idea) for i in ideas]
-    return json.dumps(levels)
-
-
-
-# PRIVATE FUNCTIONS
-
-def __get_level(ids, levels_map):
-    # Gets the level of an idea based on its sources
-    max_level = -1
-    if ids:
-        for id in ids:
-            max_level = max(max_level, levels_map[id])
-    return max_level+1
+        tags = [t.tag.tag for t in r.idea.tag_idea.select()]
+        complete_idea_map[r.idea.id] = dict(
+            tags=tags,
+            type=r.idea.origin,
+            id=r.idea.id
+        )
+        children = []
+        if r.idea.sources:
+            children = [complete_idea_map[c] for c in r.idea.sources]
+        complete_idea_map[r.idea.id]['children'] = children
+        if not r.idea.replacedBy:
+            filtered_idea_list.append(complete_idea_map[r.idea.id])
+    # return
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps(filtered_idea_list)
--- static/js/system/versioning-view.js
+++ static/js/system/versioning-view.js
@@ -15,7 +15,7 @@ class VersioningView extends View {
     load(){
         console.dir('Initializing Versioning View');
         // Clear Panel
-		$(this.container).html('<svg></svg>');
+		$(this.container).html(Mustache.render(TEMPLATES.innerVersioningViewerTemplate));
 		this.getParentContainer().addClass('loading');
 		this.getParentContainer().removeClass('empty');
 		this.setNeedsUpdate(false);
@@ -24,8 +24,7 @@ class VersioningView extends View {
             type: "GET",
             url: URL.getVersioningStructure,
             success: (data)=>{
-                var structure = JSON.parse(data);
-                this.buildVersioningPanel(structure);
+                this.buildVersioningPanel(data);
             }
         });
         return this;
@@ -36,26 +35,20 @@ class VersioningView extends View {
     */
     highlightIdeaHandler(e){
         console.dir('Custom VERSIONING Highlight idea');
-        var id = e.params.id; 
-        var idea = d3.selectAll(this.container + ' .id' + id);
-		// Hover
-		idea.classed('smallIdeaHover', true);
-		idea.transition()
-			.duration(200)
-			.attr('width', this.dimensions.smallIdea + 10)
-			.attr('height', this.dimensions.smallIdea + 10)
-			.attr('transform', 'translate(-5,-5)');
+		var id = e.params.id; 
+		var ideas = $('.' + ENV.idPrefix + id, this.getParentContainer());
+		ideas.addClass('hover');
 		// If hovering for over a given threshold of time, scroll down to the idea
-		this.hoverTimeout = window.setTimeout(()=>{ // Wait
-			var container = $(this.container); // This is the scroll container element
-			if(idea.nodes().length > 0){
-				var taskBlock = $(idea.nodes()[0]); // This is the task block that will be scrolled to
-				var scrollTo = taskBlock.offset().top - container.offset().top + container.scrollTop();
-				container.animate({
-					scrollTop: scrollTo - 30 // padding
-				}, ENV.scrollSpeed);
-			}
-		}, ENV.scrollDelay);
+		// this.hoverTimeout = window.setTimeout(()=>{ // Wait
+		// 	var container = $(this.container); // This is the scroll container element
+		// 	if(idea.nodes().length > 0){
+		// 		var taskBlock = $(idea.nodes()[0]); // This is the task block that will be scrolled to
+		// 		var scrollTo = taskBlock.offset().top - container.offset().top + container.scrollTop();
+		// 		container.animate({
+		// 			scrollTop: scrollTo - 30 // padding
+		// 		}, ENV.scrollSpeed);
+		// 	}
+		// }, ENV.scrollDelay);
     }
 
     /*
@@ -63,162 +56,139 @@ class VersioningView extends View {
     */
     blurIdeaHandler(e){
         console.dir('Custom VERSIONING Blur idea');
-        var id = e.params.id; 
-        var idea = d3.selectAll(this.container + ' .id' + id);
-		// Remove highlight
-		idea.classed('smallIdeaHover', false);
-		idea.transition()
-			.duration(200)
-			.attr('width', this.dimensions.smallIdea)
-			.attr('height', this.dimensions.smallIdea)
-			.attr('transform', 'translate(0,0)');
+        var id = e.params.id;
+        var ideas = $('.' + ENV.idPrefix + id, this.getParentContainer());
+		ideas.removeClass('hover');
 		// Clear timeout
 		clearTimeout(this.hoverTimeout);
-    }
+	}
+	
+	/*
+    Highlight ideas that have particular tags
+    */
+	highlightTagsHandler(e){
+		console.dir('Custom VERSIONING Highlight tags');
+		var tags = e.params.tags; 
+		var ideas = $(tags.join(''), this.getParentContainer());
+		ideas.addClass('hover');
+	}
 
-    /*
+	/*
+	Blur ideas that have particular tags
+	*/
+	blurTagsHandler(e){
+		console.dir('Custom VERSIONING Blur tags'); 
+		var ideas = $('.hover', this.getParentContainer());
+		ideas.removeClass('hover');
+	}
+
+	/*
     Build the visualization
     */
     buildVersioningPanel(structure){
 		if(structure.length > 0){
-			// TODO: this would probably be done better in the server
-			var maxIdeasPerLevel = -1;
-			for (var i = 0; i < structure.length; i++){
-				maxIdeasPerLevel = maxIdeasPerLevel < structure[i].length ? structure[i].length : maxIdeasPerLevel;
+			var html = '';
+			// Build view
+			for(var i = 0; i < structure.length; i++){
+				var nodeHtml = this.buildTree(structure[i]);
+				html += nodeHtml;
 			}
-	
-			// Setup dimensions, etc.
-			var margin = {top: 20, right: 20, bottom: 20, left: 30},
-				numLevels = structure.length,
-				levelWidth = 80,
-				width = levelWidth * numLevels + margin.left + margin.right,
-				levelHeight = 50,
-				height = levelHeight * maxIdeasPerLevel + margin.top + margin.bottom,
-				labelHeight = 20, // TODO figure out a better way to fit the labels on top of the chart e.g. DO A HEIGHTS DICT THAT HAS ALL HEIGHTS
-				ideaMap = {}, // Used to store the x,y coordinates of every idea
-				ideasElements = [] // Used to store the idea SVG elements for be added after the lines
-			// Generic line function, used to draw lines
-			var lineFunction = d3.line()
-					.x(function(d){return d.x;})
-					.y(function(d){return d.y;})
-	
-			// Create scale for distribution of level bands
-			var levelScale = d3.scaleBand() // Replaces scale.ordinal and rangeRoundPoints
-				.domain(UTIL.range(0,numLevels))
-				.range([0, width])
-				.paddingInner(0)
-				.paddingOuter(0);
-	
-			var scales = []
-			// Create scale for each level
-			structure.forEach(function(d,i){
-				scales.push(
-					d3.scalePoint()
-						.domain(UTIL.range(0,d.length))
-						.range([0,height]));
+			$('.treeView', this.getParentContainer()).html(html);
+			var nodes = $('.treeView .node');
+			var _this = this;
+			nodes.each(function(i,d){
+				_this.setupLines($(d));
 			});
-			
-			// Add main container
-			var chart = d3.select(this.container + ' svg')
-				.attr('width', width + margin.left)
-				.attr('height', height + margin.top + margin.bottom + 200)
-				.append('g')
-					.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-	
-			// Create containers
-			var axisLabels = chart.append('g')
-				.attr('class', 'axisLabels');
-			var axesContainer = chart.append('g')
-				.attr('transform', 'translate(0, ' + labelHeight + ')');
-	
-			// Add elements
-			var ideasArray = [];
-			for (var i = 0; i < structure.length; i++){
-				// Add labels
-				axisLabels.append('text')
-					.attr("text-anchor", "middle")
-					.text('Iteration ' + (i+1))
-					.attr('y', 0)
-					.attr('x', levelScale(i) + 20);
-	
-				// Add axis lines
-				var lineData = [
-					{x: levelScale(i), y: 0 },
-					{x: levelScale(i), y: height}];
-				axesContainer.append('path')
-					.attr('d', lineFunction(lineData))
-					.attr('class', 'axis');
-				
-				// Add connections and build ideas array
-				for (var j = 0; j < structure[i].length; j++){
-					// Setup
-					var ideaId = structure[i][j].id;
-					var connections = structure[i][j].connection.ids; // Parents
-					var connectionType = structure[i][j].connection.type;
-					var ideaPosition = 0;
-					if (!connections || connections.length === 0){
-						// There are no previous connections. Add to next available space 
-						ideaPosition = {
-							x: levelScale(i) - this.dimensions.smallIdea / 2, 
-							y: scales[i](j) - this.dimensions.smallIdea / 2
-						};
-					} else {
-						var midPoint = (ideaMap[connections[0]].y + ideaMap[connections[1]].y) / 2;
-						ideaPosition = {
-							x: levelScale(i) - this.dimensions.smallIdea / 2, 
-							y: midPoint
-						};
-					}
-					// Update ideaMap
-					ideaMap[ideaId] = ideaPosition;
-	
-					// Add lines
-					if(connections && connections.length > 0){
-						connections.forEach((d)=>{
-							// Set positions
-							var offset = this.dimensions.smallIdea / 2;
-							var lineData = [
-								{x: ideaPosition.x + offset, y: ideaPosition.y + offset },
-								{x: ideaMap[d].x + offset, y: ideaMap[d].y + offset }];
-							// Add it
-							axesContainer.append('path')
-								.attr('d', lineFunction(lineData))
-								.attr('class', 'versioningConnection ' + connectionType);
-						});
-					}
-	
-					// Add idea blocks
-					var ideaObject = { id: ideaId, position: ideaPosition }
-					ideasArray.push(ideaObject)
-				}
-			}
-			// Add ideas
-			axesContainer.selectAll('rect')
-				.data(ideasArray)
-				.enter()
-				.append('rect')
-				.attr('class', function(d){ return 'smallIdea ideaContainer hover id' + d.id; })
-				.attr('height', this.dimensions.smallIdea)
-				.attr('width', this.dimensions.smallIdea)
-				.attr('x', function(d){ return d.position.x; })
-				.attr('y', function(d){ return d.position.y; })
-				.attr('rx', 3)
-				.attr('ry', 3)
-				// Add hover effects
-				.on('mouseover', function(d){
-					// Trigger highlight event
-					$.event.trigger({type:EVENTS.highlightIdea, params:{id:d.id}});
-				}).on('mouseout', function(d){
-					// Trigger blur event
-					$.event.trigger({type:EVENTS.blurIdea, params:{id:d.id}});
-				}).on('click', function(d){
-					// TODO Load idea on click
-					VIEWS.ideasView.loadIdea(d.id);
-				});
+			// Attach hover event handlers
+			$('.treeView .smallIdea').hover(function(e){
+				var idea = $(e.target);
+				var id = idea.data('id');
+				var tags = idea.data('tags').split(',');
+				$.event.trigger({type:EVENTS.highlightIdea, params:{
+					id: id,
+					tags: tags
+				}});
+			}, function(e){
+				console.dir('>>>> BLUR');
+				var idea = $(e.target);
+				var id = idea.data('id');
+				var tags = idea.data('tags').split(',');
+				$.event.trigger({type:EVENTS.blurIdea, params:{
+					id: id,
+					tags: tags
+				}});
+			});
 		} else {
 			this.getParentContainer().addClass('empty');
 		}
 		this.getParentContainer().removeClass('loading');
 	}
 
+	buildTree(node){
+		// Build this node's html
+		var tags = node.tags.map(function(el){
+			return ENV.classPrefix + el;
+		});
+		var nodeElements = $(Mustache.render(TEMPLATES.versioningViewNodeTemplate, {
+			type: node.type,
+			classTags: tags.join(' '),
+			tags: node.tags,
+			classId: ENV.idPrefix + node.id,
+			id: node.id
+		}));
+		// Build children and attach to this node
+		var children = '';
+		if(node.children.length > 0){
+			for(var i = 0; i < node.children.length; i++){
+				children += this.buildTree(node.children[i]);
+			}
+		}
+		$('.children', nodeElements).html(children);
+		return nodeElements[0].outerHTML;
+	}
+
+	setupLines(node){
+		// Help function
+		var getCenter = function(el){
+			var offset = el.offset();
+			return {
+				top: offset.top + (el.height() / 2),
+				left: offset.left + (baseNode.width() / 2)
+			};
+		}
+		// Setup lines for this node
+		var lines = node.find(' > .line');
+		var baseNode = node.find(' > .smallIdea');
+		var basePosition = baseNode.offset();
+		var baseCenter = getCenter(baseNode);
+		var children = node.find(' > .children > .node');
+		// Connect lines to children
+		for(var i = 0; i < lines.length; i++){
+			var child = $(children[i]);
+			var line = $(lines[i]);
+			if(i < children.length){
+				// Calculate midPoint and place line there
+				var childCenter = getCenter(child);
+				var midPoint = {
+					top: childCenter.top + ((baseCenter.top - childCenter.top) / 2),
+				}
+				line.offset({
+					top: midPoint.top, 
+					left: baseCenter.left
+				});
+				// Calculate length of line
+				var a = childCenter.left - baseCenter.left;
+				var b = baseCenter.top - childCenter.top;
+				var length = Math.sqrt(a*a + b*b);
+				line.width(length);
+				// Calculate rotation
+				var opposite = b;
+				var hypotenuse = length;
+				var angle = (Math.asin(opposite / hypotenuse) * 180 / Math.PI) * -1;
+				line.css('transform', 'rotate('+angle+'deg)');
+			} else {
+				line.hide();
+			}		
+		}
+	}
 };
